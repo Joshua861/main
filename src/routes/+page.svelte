f126f034d6078a41414904bbffb889a981258c5b
Joshua861 <joshrl@proton.me>
2024-05-31 10:56:01 +0100

    first commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,73 @@
+<br /><br />
+<div class="prose prose-invert mx-auto">
+	<h1>My websites</h1>
+	<br />
+	<a href="https://words.joshrl.dev/" class="link">Wordle</a><br />
+	Wordle clone I made for fun.
+	<br /><br />
+	<a href="https://lights.joshrl.dev/" class="link">Lights Out</a><br />
+	Lights out game I made... also for fun.
+	<br /><br />
+	<a href="https://games.joshrl.dev/" class="link">Games</a><br />
+	Simple games recreated with SvelteKit.
+	<br /><br />
+	<a href="https://hn.joshrl.dev/" class="link">Hacker news reader</a><br />
+	Cool Hacker News front-end inspired by hckrnws.com.
+	<br /><br />
+	<a href="https://notes.joshrl.dev/notes" class="link">Offline notes</a><br />
+	Simple, offline, markdown, notes app
+	<br /><br />
+	<a href="https://fb.joshrl.dev/" class="link">Online notes</a><br />
+	Online notes with sync, sharing, publishing, and more. My first real project using a database and auth.
+	<br /><br />
+	<a href="https://blog.joshrl.dev/" class="link">Blog</a><br />
+	Custom blog site I made in svelte.
+	<br /><br />
+	<a href="https://party.joshrl.dev/" class="link">Party games</a><br />
+	Some simple party games you can mostly play with friends.
+	<br /><br />
+	<a href="https://long.joshrl.dev/" class="link">URL Lengthener</a><br />
+	Low effort url shortening website (except it makes the urls longer).
+	<br /><br />
+	<a href="https://rank.joshrl.dev/" class="link">Rank</a><br />
+	Rank things (at the moment only colors).
+	<br /><br />
+	<a href="https://post.joshrl.dev/" class="link">Post timeline</a><br />
+	Like Mastadon, but without any of the features. My first time using a database.
+	<br /><br />
+	<a href="https://ph.joshrl.dev/" class="link">Placeholder images</a><br />
+	placehold.co clone.
+
+	<br />
+	<hr class="my-8 h-px border-0 bg-gray-200 dark:bg-gray-700" />
+	<a href="https://popup.joshrl.dev/" class="link">Popup hell</a><br />
+	Quick joke website from forever ago that I never finished. Tons of popups.
+
+	<br /><br />
+	<a href="https://timetable.joshrl.dev/" class="link">Timetable</a><br />
+	Semi-functional timetable app using classcharts api.
+
+	<br /><br />
+	<a href="https://old-notes.joshrl.dev/notes" class="link">Old Notes</a><br />
+	Older version of the offline notes.
+
+	<hr class="my-8 h-px border-0 bg-gray-200 dark:bg-gray-700" />
+
+	<p>Thanks to my amazing friends Lucas and Ethan for helping me along my journey.</p>
+	<br />
+	Contact: <a href="mailto:joshrl@proton.me" class="link">joshrl@proton.me</a>
+</div>
+<br />
+<br />
+
+<style type="text/tailwindcss">
+	.link {
+		@apply text-blue-400 underline underline-offset-4 hover:text-blue-300;
+	}
+	* {
+		@apply font-mono text-lg sm:text-xl;
+	}
+	div {
+		@apply mx-5 sm:mx-12 md:mx-32 lg:mx-48 xl:mx-64;
+	}
+</style>
